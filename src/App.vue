51b2efc92057e424a151b967714cf14e2ea875f6
zhushukun <18618324939@163.com>
2026-09-02 16:55:48 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Welcome to Your Vue.js App</h1>
+  <img v-if="imageUrl" :src="imageUrl" alt="logo" />
+</template>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const imageUrl = ref("");
+axios.get("dev/api/breeds/image/random").then((res) => {
+  let data = res.data;
+  console.log(data);
+  imageUrl.value = data.message;
+});
+</script>
+
+<style scoped></style>
